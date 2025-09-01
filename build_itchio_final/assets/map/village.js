615d145b5cc1ab89a7d678c67beec886a48f1830
MAE009 <emmanuelmayala40@gmail.com>
2025-09-01 08:00:08 +0100

    Create village.js

---
+++ build_itchio_final/assets/map/village.js
@@ -0,0 +1,56 @@
+{
+    "compressionlevel": -1,
+    "height": 19,
+    "infinite": false,
+    "layers": [
+        {
+            "data": [1,1,1,1,1,1,1,1,...], // vos données de tiles
+            "height": 19,
+            "id": 1,
+            "name": "background",
+            "opacity": 1,
+            "type": "tilelayer",
+            "visible": true,
+            "width": 25,
+            "x": 0,
+            "y": 0
+        },
+        {
+            "data": [0,0,0,0,1,1,0,0,...], // calque de collisions
+            "height": 19,
+            "id": 2,
+            "name": "collision",
+            "opacity": 1,
+            "type": "tilelayer",
+            "visible": true,
+            "width": 25,
+            "x": 0,
+            "y": 0
+        }
+    ],
+    "nextlayerid": 3,
+    "nextobjectid": 1,
+    "orientation": "orthogonal",
+    "renderorder": "right-down",
+    "tiledversion": "1.9.2",
+    "tileheight": 32,
+    "tilesets": [
+        {
+            "columns": 8,
+            "firstgid": 1,
+            "image": "my_tileset.png", // dans assets/tilesets/
+            "imageheight": 256,
+            "imagewidth": 256,
+            "margin": 0,
+            "name": "my_tileset",
+            "spacing": 0,
+            "tilecount": 64,
+            "tileheight": 32,
+            "tilewidth": 32
+        }
+    ],
+    "tilewidth": 32,
+    "type": "map",
+    "version": "1.9",
+    "width": 25
+}
